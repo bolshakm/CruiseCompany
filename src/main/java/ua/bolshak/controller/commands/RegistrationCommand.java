ce254913797a3b36a529279cf8b2c0a65c6a0b4d
Maksym <bolshakmv@gmail.com>
2018-05-15 01:48:31 +0300

    Added validation for registration

--- src/main/java/ua/bolshak/controller/commands/RegistrationCommand.java
+++ src/main/java/ua/bolshak/controller/commands/RegistrationCommand.java
@@ -4,6 +4,7 @@ import ua.bolshak.model.entity.User;
 import ua.bolshak.model.service.RoleService;
 import ua.bolshak.model.service.ShipService;
 import ua.bolshak.model.service.UserService;
+import ua.bolshak.properties.RegExResources;
 import ua.bolshak.properties.RequestParams;
 import ua.bolshak.properties.TextResources;
 
@@ -11,10 +12,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class RegistrationCommand implements ICommand{
     private static RequestParams params = RequestParams.getInstance();
     private static TextResources text = TextResources.getInstance();
+    private static RegExResources regExResources = RegExResources.getInstance();
+    private static final String LOGIN_REGEX = regExResources.getProperty("login.regexp");
+    private static final String PASSWORD_REGEX = regExResources.getProperty("password.regexp");
+    private static final String NAME_REGEX = regExResources.getProperty("name.regexp");
+    private static final String LAST_NAME_REGEX = regExResources.getProperty("last.name.regexp");
+    private static final String EMAIL_REGEX = regExResources.getProperty("email.regexp");
+    private static final String WRONG_INPUT = text.getProperty("wrong.input");
     private static final String WRONG_LOGIN = text.getProperty("wrong.login");
     private static final String WRONG_PASSWORD = text.getProperty("wrong.password");
     private static final String WRONG_EMAIL = text.getProperty("wrong.email");
@@ -29,6 +38,11 @@ public class RegistrationCommand implements ICommand{
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Pattern loginPattern = Pattern.compile(LOGIN_REGEX);
+        Pattern passwordPattern = Pattern.compile(PASSWORD_REGEX);
+        Pattern namePattern = Pattern.compile(NAME_REGEX);
+        Pattern lastNamePattern = Pattern.compile(LAST_NAME_REGEX);
+        Pattern emailPattern = Pattern.compile(EMAIL_REGEX);
         User user = new User();
         String login = request.getParameter(LOGIN);
         String password = request.getParameter(PASSWORD);
@@ -36,42 +50,59 @@ public class RegistrationCommand implements ICommand{
         String name = request.getParameter(NAME);
         String lastName = request.getParameter(LAST_NAME);
         String email = request.getParameter(EMAIL);
-        user.setLogin(login);
-        user.setPassword(password);
-        user.setName(name);
-        user.setLastName(lastName);
-        user.setEmail(email);
+        if (loginPattern.matcher(login).matches()) {
+            if (UserService.findByLogin(login) == null) {
+                user.setLogin(login);
+            } else {
+                return redirectToRegistrationPageWithErrorMassage(request, response, WRONG_LOGIN, user);
+            }
+        } else {
+            return redirectToRegistrationPageWithErrorMassage(request, response, WRONG_INPUT, user);
+        }
+        if (passwordPattern.matcher(PASSWORD_REGEX).matches()) {
+            if (password.equals(passwordConfirm)) {
+                user.setPassword(password);
+            } else {
+                return redirectToRegistrationPageWithErrorMassage(request, response, WRONG_PASSWORD, user);
+            }
+        } else {
+            return redirectToRegistrationPageWithErrorMassage(request, response, WRONG_INPUT, user);
+
+        }
+        if (namePattern.matcher(NAME_REGEX).matches()) {
+            user.setName(name);
+        } else {
+            return redirectToRegistrationPageWithErrorMassage(request, response, WRONG_INPUT, user);
+        }
+        if (lastNamePattern.matcher(LAST_NAME_REGEX).matches()) {
+            user.setLastName(lastName);
+        } else {
+            return redirectToRegistrationPageWithErrorMassage(request, response, WRONG_INPUT, user);
+        }
+        if (emailPattern.matcher(EMAIL_REGEX).matches()) {
+            if (UserService.findByEmail(email) == null) {
+                user.setEmail(email);
+            } else {
+                return redirectToRegistrationPageWithErrorMassage(request, response, WRONG_EMAIL, user);
+            }
+        } else {
+            return redirectToRegistrationPageWithErrorMassage(request, response, WRONG_INPUT, user);
+        }
         user.setRole(RoleService.findById(2));
         user.setShip(ShipService.getEmptyShip());
-        if (!password.equals(passwordConfirm)){
-            request.setAttribute(ERROR_MASSAGE, WRONG_PASSWORD);
-            request.setAttribute(LOGIN, user.getLogin());
-            request.setAttribute(PASSWORD_CONFIRM, user.getPassword());
-            request.setAttribute(NAME, user.getName());
-            request.setAttribute(LAST_NAME, user.getLastName());
-            request.setAttribute(EMAIL, user.getEmail());
-            return new ToRegistrationPage().execute(request, response);
-        }
-        if (UserService.findByLogin(login) != null){
-            request.setAttribute(ERROR_MASSAGE, WRONG_LOGIN);
-            request.setAttribute(PASSWORD, user.getPassword());
-            request.setAttribute(PASSWORD_CONFIRM, user.getPassword());
-            request.setAttribute(NAME, user.getName());
-            request.setAttribute(LAST_NAME, user.getLastName());
-            request.setAttribute(EMAIL, user.getEmail());
-            return new ToRegistrationPage().execute(request, response);
-        }
-        if (UserService.findByEmail(email) != null){
-            request.setAttribute(ERROR_MASSAGE, WRONG_EMAIL);
-            request.setAttribute(LOGIN, user.getLogin());
-            request.setAttribute(PASSWORD, user.getPassword());
-            request.setAttribute(PASSWORD_CONFIRM, user.getPassword());
-            request.setAttribute(NAME, user.getName());
-            request.setAttribute(LAST_NAME, user.getLastName());
-            return new ToRegistrationPage().execute(request, response);
-        }
         UserService.add(user);
         request.getSession().setAttribute(USER, user);
         return new ToMainPage().execute(request, response);
     }
+
+    private String redirectToRegistrationPageWithErrorMassage(HttpServletRequest request,HttpServletResponse response, String errorMassage, User user) throws IOException, ServletException {
+        request.setAttribute(LOGIN, user.getLogin());
+        request.setAttribute(PASSWORD_CONFIRM, user.getPassword());
+        request.setAttribute(NAME, user.getName());
+        request.setAttribute(LAST_NAME, user.getLastName());
+        request.setAttribute(EMAIL, user.getEmail());
+        request.setAttribute(ERROR_MASSAGE, errorMassage);
+        return new ToRegistrationPage().execute(request, response);
+    }
+
 }
